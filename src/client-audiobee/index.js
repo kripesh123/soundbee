49f4914e438498351aff3251a4044ef267a9cf0b
kripesh <kripeshbista3@gmail.com>
2021-05-30 21:55:41 +0545

    Update Ajax Client.

--- src/client-audiobee/index.js
+++ src/client-audiobee/index.js
@@ -5,6 +5,8 @@ import TestAnswers from "./api/tests/answers";
 import TestTakes from "./api/tests/takes";
 import TestTakeAnswers from "./api/tests/takeAnswers";
 import ApiClient from "./apiClient";
+import AjaxLogin from "./api/ajaxLogin";
+import AjaxRegister from "./api/ajaxRegister";
 
 export default class Client {
     constructor(options = {}) {
@@ -22,6 +24,15 @@ export default class Client {
         this.answers = new TestAnswers(apiClient);
         this.takes = new TestTakes(apiClient);
         this.takeAnswers = new TestTakeAnswers(apiClient);
+        
+        this.ajax = {};
+        this.ajax.tests = new new Tests(ajaxClient);
+        this.ajax.login = new AjaxLogin(ajaxClient);
+        this.ajax.register = new AjaxRegister(ajaxClient);
+        this.ajax.questions = new TestQuestions(ajaxClient);
+        this.ajax.answers = new TestAnswers(ajaxClient);
+        this.ajax.takes = new TestTakes(ajaxClient);
+        this.ajax.takeAnswers = new TestTakeAnswers(ajaxClient);
     }
 
     static authorize = (baseUrl, email) => ApiClient.authorize(baseUrl, email);
