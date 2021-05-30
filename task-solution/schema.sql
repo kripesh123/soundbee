feae5168eac6417195bbc7e99c003a8c19fa0492
kripesh <kripeshbista3@gmail.com>
2021-05-30 21:02:07 +0545

    Add Table: worker, test_set, test_question, test_answer, take, take_answers.

--- task-solution/schema.sql
+++ task-solution/schema.sql
@@ -15,6 +15,19 @@ CREATE TABLE `audiobee_task`.`user` (
   UNIQUE INDEX `uq_mobile` (`mobile` ASC),
   UNIQUE INDEX `uq_email` (`email` ASC));
 
+CREATE TABLE `audiobee_task`.`worker` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `firstName` VARCHAR(50) NULL DEFAULT NULL,
+  `lastName` VARCHAR(50) NULL DEFAULT NULL,
+  `mobile` VARCHAR(15) NULL,
+  `email` VARCHAR(50) NULL,
+  `password` VARCHAR(32) NOT NULL,
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `uq_mobile` (`mobile` ASC),
+  UNIQUE INDEX `uq_email` (`email` ASC));
+
 CREATE TABLE `audiobee_task`.`language` (
     `id` BIGINT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(75) NOT NULL,
@@ -33,18 +46,29 @@ CREATE TABLE `audiobee_task`.`type` (
     PRIMARY KEY (`id`),
     UNIQUE INDEX `uq_name` (`name` ASC));
 
+CREATE TABLE `audiobee_task`.`test_set` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50) NOT NULL,
+  `description` VARCHAR(50) NOT NULL,
+  `attempt` SMALLINT(6) NOT NULL DEFAULT 0,
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`id`));
 
 CREATE TABLE `audiobee_task`.`test` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
   `userId` BIGINT NOT NULL,
+  `workerId` BIGINT NOT NULL,
   `languageId`BIGINT NOT NULL,
   `categoryId`BIGINT NOT NULL,
   `typeId` BIGINT NOT NULL,
+  `testSetId` BIGINT NOT NULL,
   `title` VARCHAR(75) NOT NULL,
   `metaTitle` VARCHAR(100) NULL,
   `slug` VARCHAR(100) NOT NULL,
   `summary` TINYTEXT NULL,
   `score` SMALLINT(6) NOT NULL DEFAULT 0,
+  `position` SMALLINT(6) NOT NULL DEFAULT 0,
   `createdAt` DATETIME NOT NULL,
   `updatedAt` DATETIME NULL DEFAULT NULL,
   `content` TEXT NULL DEFAULT NULL,
@@ -57,6 +81,14 @@ CREATE TABLE `audiobee_task`.`test` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION); 
 
+ALTER TABLE `audiobee_task`.`test`
+ADD INDEX `idx_test_worker` (`workerId` ASC);
+ALTER TABLE `audiobee_task`.`test`
+ADD CONSTRAINT `fk_test_worker`
+    FOREIGN KEY (`workerId`)
+    REFERENCES `audiobee_task`.`worker` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION;
 
 ALTER TABLE `audiobee_task`.`test`
 ADD INDEX `idx_test_language` (`languageId` ASC);
@@ -84,3 +116,121 @@ ADD CONSTRAINT `fk_test_type`
     REFERENCES `audiobee_task`.`type` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION;
+
+ALTER TABLE `audiobee_task`.`test`
+ADD INDEX `idx_test_test_set` (`testSetId` ASC);
+ALTER TABLE `audiobee_task`.`test`
+ADD CONSTRAINT `fk_test_test_set`
+    FOREIGN KEY (`testSetId`)
+    REFERENCES `audiobee_task`.`test_set` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION;
+
+CREATE TABLE `audiobee_task`.`test_question` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `testId` BIGINT NOT NULL,
+  `type` VARCHAR(50) NOT NULL,
+  `active` TINYINT(1) NOT NULL DEFAULT 0,
+  `level` SMALLINT(6) NOT NULL DEFAULT 0,
+  `score` SMALLINT(6) NOT NULL DEFAULT 0,
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NULL DEFAULT NULL,
+  `content` TEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `idx_question_test` (`testId` ASC),
+  CONSTRAINT `fk_question_test`
+    FOREIGN KEY (`testId`)
+    REFERENCES `audiobee_task`.`test` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+CREATE TABLE `audiobee_task`.`test_answer` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `testId` BIGINT NOT NULL,
+  `questionId` BIGINT NOT NULL,
+  `active` TINYINT(1) NOT NULL DEFAULT 0,
+  `correct` TINYINT(1) NOT NULL DEFAULT 0,
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NULL DEFAULT NULL,
+  `content` TEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `idx_answer_test` (`testId` ASC),
+  CONSTRAINT `fk_answer_test`
+    FOREIGN KEY (`testId`)
+    REFERENCES `audiobee_task`.`test` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+ALTER TABLE `audiobee_task`.`test_answer` 
+ADD INDEX `idx_answer_question` (`questionId` ASC);
+ALTER TABLE `audiobee_task`.`test_answer` 
+ADD CONSTRAINT `fk_answer_question`
+  FOREIGN KEY (`questionId`)
+  REFERENCES `audiobee_task`.`test_question` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+CREATE TABLE `audiobee_task`.`take` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `workerId` BIGINT NOT NULL,
+  `testId` BIGINT NOT NULL,
+  `status` SMALLINT(6) NOT NULL DEFAULT 0,
+  `score` SMALLINT(6) NOT NULL DEFAULT 0,
+  `published` TINYINT(1) NOT NULL DEFAULT 0,
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NULL DEFAULT NULL,
+  `startedAt` DATETIME NULL DEFAULT NULL,
+  `finishedAt` DATETIME NULL DEFAULT NULL,
+  `content` TEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `idx_take_worker` (`workerId` ASC),
+  CONSTRAINT `fk_take_worker`
+    FOREIGN KEY (`workerId`)
+    REFERENCES `audiobee_task`.`worker` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+ALTER TABLE `audiobee_task`.`take` 
+ADD INDEX `idx_take_test` (`testId` ASC);
+ALTER TABLE `audiobee_task`.`take` 
+ADD CONSTRAINT `fk_take_test`
+  FOREIGN KEY (`testId`)
+  REFERENCES `audiobee_task`.`test` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+CREATE TABLE `audiobee_task`.`take_answer` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `takeId` BIGINT NOT NULL,
+  `questionId` BIGINT NOT NULL,
+  `answerId` BIGINT NOT NULL,
+  `active` TINYINT(1) NOT NULL DEFAULT 0,
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NULL DEFAULT NULL,
+  `content` TEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `idx_answer_take` (`takeId` ASC),
+  CONSTRAINT `fk_answer_take`
+    FOREIGN KEY (`takeId`)
+    REFERENCES `audiobee_task`.`take` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+ALTER TABLE `audiobee_task`.`take_answer` 
+ADD INDEX `idx_tanswer_question` (`questionId` ASC);
+ALTER TABLE `audiobee_task`.`take_answer` 
+ADD CONSTRAINT `fk_tanswer_question`
+  FOREIGN KEY (`questionId`)
+  REFERENCES `audiobee_task`.`test_question` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `audiobee_task`.`take_answer` 
+ADD INDEX `idx_tanswer_answer` (`answerId` ASC);
+ALTER TABLE `audiobee_task`.`take_answer` 
+ADD CONSTRAINT `fk_tanswer_answer`
+  FOREIGN KEY (`answerId`)
+  REFERENCES `audiobee_task`.`test_answer` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
