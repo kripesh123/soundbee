a3c22d993d14cc40a397010d7a558e4f1c0e416d
kripesh <kripeshbista3@gmail.com>
2021-05-30 16:04:54 +0545

    dummy

--- seeders/008-add-take.js
+++ seeders/008-add-take.js
@@ -3,14 +3,14 @@
 module.exports = {
     up: (queryInterface, Sequelize) => {
         return queryInterface.bulkInsert('takes', [
-            { testId:1, workerId: 1, content:'Good', score:15, isActive:true, createdAt: new Date(), updatedAt: new Date()},
-            { testId:1, workerId: 2, content:'Good', score:10, isActive:true, createdAt: new Date(), updatedAt: new Date()},
-            { testId:2, workerId: 1, content:'Bad', score:5, isActive:true, createdAt: new Date(), updatedAt: new Date()},
-            { testId:2, workerId: 2, content:'Good', score:8, isActive:true, createdAt: new Date(), updatedAt: new Date()},
-            { testId:3, workerId: 1, content:'Poor', score:5, isActive:true, createdAt: new Date(), updatedAt: new Date()},
-            { testId:3, workerId: 2, content:'Perfect', score:20, isActive:true, createdAt: new Date(), updatedAt: new Date()},
-            { testId:4, workerId: 1, content:'Good', score:6, isActive:true, createdAt: new Date(), updatedAt: new Date()},
-            { testId:4, workerId: 2, content:'Perfect', score:10, isActive:true, createdAt: new Date(), updatedAt: new Date()},           
+            { testId:1, workerId: 1, content:'PASS', score:15, isActive:true, createdAt: new Date(), updatedAt: new Date()},
+            { testId:1, workerId: 2, content:'PASS', score:10, isActive:true, createdAt: new Date(), updatedAt: new Date()},
+            { testId:2, workerId: 1, content:'FAIL', score:5, isActive:true, createdAt: new Date(), updatedAt: new Date()},
+            { testId:2, workerId: 2, content:'PASS', score:8, isActive:true, createdAt: new Date(), updatedAt: new Date()},
+            { testId:3, workerId: 1, content:'FAIL', score:5, isActive:true, createdAt: new Date(), updatedAt: new Date()},
+            { testId:3, workerId: 2, content:'PASS', score:20, isActive:true, createdAt: new Date(), updatedAt: new Date()},
+            { testId:4, workerId: 1, content:'PASS', score:6, isActive:true, createdAt: new Date(), updatedAt: new Date()},
+            { testId:4, workerId: 2, content:'PASS', score:10, isActive:true, createdAt: new Date(), updatedAt: new Date()},           
         ])
     },
 
